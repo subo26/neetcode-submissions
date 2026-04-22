6496d87790dfbf887e87cd7b348417133a2727fb
subo26 <36163552+subo26@users.noreply.github.com>
2026-04-23 00:00:23 +0530

    Delete Data Structures & Algorithms/duplicate-integer/submission-5.kt
    deleted

--- Data Structures & Algorithms/duplicate-integer/submission-5.kt
+++
@@ -1,9 +0,0 @@
-class Solution {
-    fun hasDuplicate(nums: IntArray): Boolean {
-        val seen = HashSet<Int>(nums.size)
-        for (n in nums) {
-            if (!seen.add(n)) return true
-        }
-        return false
-    }
-}
