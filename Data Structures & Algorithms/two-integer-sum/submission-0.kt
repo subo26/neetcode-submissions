bc68ce2c3e3aa25cf64c31bc88a27540e7609af6
subo26 <36163552+subo26@users.noreply.github.com>
2026-04-23 12:59:05 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val array = mutableListOf<Int>()
+        for( i in nums.indices){
+            for (j in i+1 until nums.size){
+                if(nums[i] + nums[j] == target){
+                    array.add(i)
+                    array.add(j)
+                }
+            }
+        }
+        return array.toIntArray()
+    }
+}
